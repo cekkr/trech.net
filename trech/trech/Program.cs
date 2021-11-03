6a5865e1580facd85752424e38fed5406e253712
Riccardo Cecchini <rcecchini.ds@gmail.com>
2021-11-03 17:06:29 +0100

    First commit

---
+++ trech/trech/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace trech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ trech/trechLib/trech.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace trechLib
+{
+    public class trech
+    {
+
+    }
+}
